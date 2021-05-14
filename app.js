f22ed8f4be76f642ff3f5c4cb42a76676702c787
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2021-05-14 14:16:25 -0400

    static file services created

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ const path = require('path');
 const PORT = process.env.PORT || 8000;
 const app = express();
 
-app.use(morgan('combined')); // HTTP request logging middleware for Node 
-app.use(express.static(path.join(__dirname, "/public"))); // Tells express this is where we are keeping out static files
+app.use(morgan('tiny')); // HTTP request logging middleware for Node 
+app.use(express.static(path.join(__dirname, "/public/"))); // Tells express this is where we are keeping out static files
+
+app.use("/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")));
+app.use("/js", express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")));
+app.use("/js", express.static(path.join(__dirname, "node_modules/jquery/dist")));
+
 app.get('/', (req, res) => {
     // res.send("Hello Express & Node");
-    res.sendFile(path.join(__dirname, "/views/index.html")); //__dirname = location of current excecutable
+    res.sendFile(path.join(__dirname, "/views", "/index.html")); //__dirname = location of current excecutable
 });
 
 app.listen(PORT, () => {
