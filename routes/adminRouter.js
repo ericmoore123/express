89d3c348fece4f97df5b0915ce53714d9899a624
Eric Moore <51104403+ericmoore123@users.noreply.github.com>
2021-07-31 14:46:39 -0400

    admin router creation and setup

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const adminRouter = express.Router();
+const mssql = require('mssql');
 
-const router = (nav) => {
+const router = (/*nav*/) => {
+
+    const request = new mssql.Request();
+
     adminRouter.route('/')
-        .get((req, res) => {
-            res.send("Inserting Books!");
+        .get( async (req, res) => {
+            const result = await request.query('select * from books');
+            res.json(result);
         });
     return adminRouter;
 }
